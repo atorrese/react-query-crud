a51e062c8133dda5455e2673c7da7b05f8e7f89d
Angel Torres <atorrese@unemi.edu.ec>
2023-04-02 17:43:37 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ProductForm from "./components/ProductForm";
+import Products from "./components/Products";
+export default function App() {
+  return (
+    <>
+      <ProductForm/>
+      <Products/>
+    </>
+  );
+}
